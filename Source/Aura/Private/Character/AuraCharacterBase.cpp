991f82bfb4017283b96c3a09363024983a1d0e36
leehyvaa <k05n17j@gmail.com>
2024-03-16 17:42:57 +0900

    Feat: 캐릭터만들기

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -8,6 +8,8 @@ AAuraCharacterBase::AAuraCharacterBase()
  	PrimaryActorTick.bCanEverTick = false;
 
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
